cdb9f159568af9df5c988fd7eb5f9e3304b8f070
MajdVictor <majd.hafiri@doctolib.com>
2023-03-24 17:36:42 +0100

    Add user creation feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   
   root to: "main#index"
   get "about", to: "about#index"
+  
+  get "sign_up", to: "registration#new"
+  post "sign_up", to: "registration#create"
 
 end
