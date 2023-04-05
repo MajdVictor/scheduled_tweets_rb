9a6079749d87d90800d83f13a9567c15cf2763fc
MajdVictor <majd.hafiri@doctolib.com>
2023-04-05 16:29:49 +0200

    add pry for debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
+# require 'pry'
 class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil 
+        binding.pry
         redirect_to root_path, notice: "Logged out!"
     end
 end
